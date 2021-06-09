379f74f1ed7bb0414334ec6f9adf25f27f8a9308
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2021-06-09 16:07:29 -0500

    cambios pendjos

--- app/src/main/java/com/example/ama/MainActivity.java
+++ app/src/main/java/com/example/ama/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.ama;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button consBtn, repBtn, mapBtn, pBtn;
+    //Para separar la barra de busqueda
+    private Button perfilBtn, reportBtn, homeBtn, puntosBtn, consejosBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
