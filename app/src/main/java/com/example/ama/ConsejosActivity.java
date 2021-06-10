b09fc9872ceb55db84ee1541369c160dc7449a0d
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2021-06-09 20:10:04 -0500

    perfil

--- app/src/main/java/com/example/ama/ConsejosActivity.java
+++ app/src/main/java/com/example/ama/ConsejosActivity.java
@@ -3,12 +3,25 @@ package com.example.ama;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ConsejosActivity extends AppCompatActivity {
+
+    public Button reportBtn2, homeBtn2, puntosBtn2, perfilBtn2, consejosBtn2;
+    public TextView nickTv, advTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consejos);
+
+        reportBtn2 = findViewById(R.id.reportBtn2);
+        homeBtn2 = findViewById(R.id.homeBtn2);
+        puntosBtn2 = findViewById(R.id.puntosBtn2);
+        perfilBtn2 = findViewById(R.id.perfilBtn2);
+        consejosBtn2 = findViewById(R.id.consejosBtn2);
+        nickTv = findViewById(R.id.nickTv);
+        advTv = findViewById(R.id.advTv);
     }
 }
--- app/src/main/java/com/example/ama/MainActivity.java
+++ app/src/main/java/com/example/ama/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.ama;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button consBtn, repBtn, mapBtn, pBtn;
     //Para separar la barra de busqueda
@@ -16,5 +18,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        consBtn = findViewById(R.id.consBtn);
+        repBtn = findViewById(R.id.repBtn);
+        mapBtn = findViewById(R.id.mapBtn);
+        pBtn = findViewById(R.id.pBtn);
+        perfilBtn = findViewById(R.id.perfilBtn);
+        reportBtn = findViewById(R.id.reportBtn);
+        homeBtn = findViewById(R.id.homeBtn);
+        puntosBtn = findViewById(R.id.puntosBtn);
+        consejosBtn = findViewById(R.id.consejosBtn);
+
+
+        consBtn.setOnClickListener(this);
+        repBtn.setOnClickListener(this);
+        mapBtn.setOnClickListener(this);
+        pBtn.setOnClickListener(this);
+
+        perfilBtn.setOnClickListener(this);
+        reportBtn.setOnClickListener(this);
+        homeBtn.setOnClickListener(this);
+        puntosBtn.setOnClickListener(this);
+        consejosBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.consBtn:
+                Intent a = new Intent(this, ConsejosActivity.class);
+                startActivity(a);
+        }
+
     }
 }
